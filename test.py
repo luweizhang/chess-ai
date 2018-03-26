8d4d8f5fd1c70fdbe374f7646c7dde485c482843
luweizhang <luwzhang@gmail.com>
2018-03-25 18:46:13 -0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,37 @@
+"""tests go here"""
+
+import unittest
+
+def add(a, b):
+ 
+    """This function adds two numbers a, b and returns their sum
+ 
+ 
+ 
+    a and b may integers
+ 
+    """
+ 
+    if isinstance(a, int) and isinstance(b, int):
+ 
+        return a + b
+ 
+    elseif isinstance(a, str) and isinstance(b, str):
+ 
+        return int(a) + intg(b)
+ 
+    else:
+ 
+        raise Exception('Invalid arguments')
+ 
+ 
+ 
+class Test(unittest.TestCase):
+ 
+    def test_add(self):
+ 
+        self.assertEqual(5, add(2, 3))
+ 
+        self.assertEqual(15, add(-6, 21))
+ 
+        self.assertRaises(Exception, add, 4.0, 5.0)
